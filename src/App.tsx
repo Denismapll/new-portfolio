45fd57d428ac931d6a9854f0612d606d042e3f45
DENIS DE FERRAZ <denis.black65@gmail.com>
2025-05-12 10:50:43 -0300

    ajuste no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import React from 'react'
-// import { useEffect } from 'react'
-import { Footer } from './components/Footer/Footer'
-import { Header } from './components/Header/Header'
-import { Main } from './components/Main/Main'
-import { GlobalStyle } from './styles/global'
-import { Analytics } from "@vercel/analytics/react"
+import React from 'react';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { Main } from './components/Main/Main';
+import { GlobalStyle } from './styles/global'; // Importe o GlobalStyle
+import { Analytics } from "@vercel/analytics/react";
+import 'react-toastify/dist/ReactToastify.css';
 
-import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
     <>
-      <GlobalStyle></GlobalStyle>
-      <Header></Header>
-      <Main></Main>
+      <GlobalStyle /> {/* ← Esta é a forma CORRETA */}
+      <Header />
+      <Main />
       <Analytics />
-      <Footer></Footer>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
